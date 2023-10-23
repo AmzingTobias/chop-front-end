3d8effbebeb8a58564c5d1bb310c6dc9cc0b3d1f
Tobias <tobiasdunn.02@gmail.com>
2023-10-23 22:34:20 +0100

    Adjust load in of website title font

--- app/components/navbars/common/Website-Title.tsx
+++ app/components/navbars/common/Website-Title.tsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import { Raleway } from "next/font/google";
-const raleway = Raleway({ subsets: ["latin"], weight: "800" });
+const raleway = Raleway({ subsets: ["latin"] });
 
 const WebsiteTitle = () => {
   return (
     <div className="w-full">
       <h1
-        className={`text-5xl text-white hover:text-gray-300 text-center ${raleway.className}`}
+        className={`text-5xl text-white font-extrabold hover:text-gray-300 text-center ${raleway.className}`}
       >
         chop
       </h1>
