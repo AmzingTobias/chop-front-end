bfafe55110d0e51ae14c4b96cebcff2471971a20
Tobias <tobiasdunn.02@gmail.com>
2023-10-23 17:38:03 +0100

    Fix path for titlebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Titlebar from "@/app/components/navbars/Titlebar";
+import Titlebar from "./components/navbars/Titlebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
