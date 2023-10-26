c8969d900a4260642ca591fb75acc692c8909295
Tobias <tobiasdunn.02@gmail.com>
2023-10-26 21:26:36 +0100

    Change heart button to use useState for demo

--- app/components/product-cards/common/ProductFavouriteBtn.tsx
+++ app/components/product-cards/common/ProductFavouriteBtn.tsx
@@ -1,21 +1,26 @@
+"use client";
+
+import { useState } from "react";
 import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 
 const ProductFavouriteBtn = () => {
   //TODO Load from API
-  const isFavourite = false;
+  const [isFavourite, setIsFavourite] = useState(false);
 
   return (
-    <div className="relative flex justify-center  cursor-default bg-gray-100 rounded-full p-1.5">
+    <div
+      onClick={(event) => {
+        event.preventDefault();
+        setIsFavourite((prevToggle) => !prevToggle);
+      }}
+      className="relative flex justify-center  cursor-default bg-gray-100 rounded-full p-1.5 text-red-500"
+    >
       <AiFillHeart
-        className={`absolute hover:opacity-100 text-red-500 ${
+        className={`absolute hover:opacity-100 ${
           isFavourite ? "opacity-100" : "opacity-0"
         }`}
       />
-      <AiOutlineHeart
-        className={`text-red-500 hover:opacity-0 ${
-          isFavourite ? "opacity-0" : ""
-        }`}
-      />
+      <AiOutlineHeart />
     </div>
   );
 };
