3a4ea5d705bcb7bbc225cf092fe2115d7c333e9b
Tobias <tobiasdunn.02@gmail.com>
2023-11-14 18:26:16 +0000

    Split into components

---
+++ app/(account)/settings/SettingOptionHeader.tsx
@@ -0,0 +1,5 @@
+const SettingOptionHeader: React.FC<{ header: string }> = ({ header }) => {
+  return <h1 className="self-center text-4xl font-semibold mt-4">{header}</h1>;
+};
+
+export default SettingOptionHeader;
--- app/(account)/settings/SettingOptions.tsx
+++ app/(account)/settings/SettingOptions.tsx
@@ -1,5 +1,7 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { ReactNode } from "react";
+import Welcome from "./Welcome";
+import SettingOptionHeader from "./SettingOptionHeader";
 
 export type TSettingOptions = {
   optionDisplayName: string;
@@ -17,26 +19,23 @@ export const SettingOptions: React.FC<{ settingTabs: TSettingOptions[] }> = ({
       orientation="vertical"
     >
       <TabsList className="sm:basis-1/4 p-0 w-[200px] bg-transparent">
-        <div className="basis-1/4 bg-slate-200 w-full min-h-[112px] mb-2 rounded-sm hidden md:flex">
-          <div className="flex self-center items-center gap-2 m-4">
-            <div className="bg-gradient-to-t from-indigo-900 to-blue-900 w-[80px] h-[80px] rounded-full"></div>
-            <div className="text-black font-bold mt-2">
-              <p>Hi,</p>
-              <p>email@email.co.uk</p>
-            </div>
-          </div>
+        <div className="basis-1/4 bg-slate-200 w-full min-h-[112px] mb-2 rounded-sm hidden md:flex shadow-sm">
+          <Welcome />
         </div>
-        <div className="bg-slate-200 p-2 w-full rounded-sm">
+        <div className="bg-slate-200 p-2 w-full rounded-sm shadow-sm">
           {settingTabs.map((settingTab, index) => (
             <TabsTrigger
-              className="w-full text-lg"
+              className="w-full text-lg hover:bg-white hover:bg-opacity-20"
               value={`${index}`}
               key={index}
             >
               {settingTab.optionDisplayName}
             </TabsTrigger>
           ))}
-          <TabsTrigger className="w-full text-lg" value="logout">
+          <TabsTrigger
+            className="w-full text-lg rounded-sm hover:bg-white hover:bg-opacity-20"
+            value="logout"
+          >
             Logout
           </TabsTrigger>
         </div>
@@ -44,12 +43,10 @@ export const SettingOptions: React.FC<{ settingTabs: TSettingOptions[] }> = ({
       <div className="mt-4 sm:mt-0 sm:mx-4 sm:basis-3/4 sm:w-full ">
         {settingTabs.map((setting, index) => (
           <TabsContent value={`${index}`} key={index} className="mt-0">
-            <div className="min-h-[96px] bg-slate-200 mb-2 rounded-sm p-2 flex">
-              <h1 className="self-center text-4xl font-semibold mt-4">
-                {setting.optionHeaderName}
-              </h1>
+            <div className="min-h-[96px] bg-slate-200 mb-2 rounded-sm p-2 flex shadow-sm">
+              <SettingOptionHeader header={setting.optionHeaderName} />
             </div>
-            <div className="bg-slate-200 min-h-full rounded-sm p-2">
+            <div className="bg-slate-200 min-h-full rounded-sm p-2 shadow-sm">
               {setting.optionContent}
             </div>
           </TabsContent>
