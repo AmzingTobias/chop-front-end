f961cce4671b24aa11c37fa7a4f5e43809250c39
Tobias <tobiasdunn.02@gmail.com>
2023-11-30 16:53:34 +0000

    Add variant to search bar to use accent colour

--- app/components/searchbars/Searchbar.tsx
+++ app/components/searchbars/Searchbar.tsx
@@ -5,19 +5,35 @@ import { FaSearch } from "react-icons/fa";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
-export function Searchbar() {
+interface ISearchbarProps {
+  variant?: "secondary" | "accent";
+}
+
+export const Searchbar: React.FC<ISearchbarProps> = ({
+  variant = "secondary",
+}) => {
   return (
     <div className="flex items-center w-full space-x-2">
       <Input
         type="text"
-        className="bg-transparent text-background"
+        className={`bg-transparent ${
+          variant === "accent" ? "border-accent focus-visible:ring-accent" : ""
+        }`}
         placeholder="Search"
       />
-      <Button type="submit" variant={"secondary"}>
-        <FaSearch className="text-lg text-accent" />
+      <Button
+        type="submit"
+        className={`${variant === "accent" ? "bg-accent" : ""}`}
+        variant={"secondary"}
+      >
+        <FaSearch
+          className={`text-lg ${
+            variant === "accent" ? "text-accent-foreground" : "text-accent"
+          }`}
+        />
       </Button>
     </div>
   );
-}
+};
 
 export default Searchbar;
