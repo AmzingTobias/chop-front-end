419c69f79607abe95ef838905aa82e3a4d224344
Tobias <tobiasdunn.02@gmail.com>
2023-12-12 15:24:03 +0000

    Add functionality for storing basket in local storage

--- app/components/product-cards/common/AddToBasketBtn.tsx
+++ app/components/product-cards/common/AddToBasketBtn.tsx
@@ -1,8 +1,39 @@
-import { Button } from "@/components/ui/button";
+"use client";
 
-const AddToBasketBtn = () => {
+import { addProductToLocalBasket } from "@/app/data/basket";
+import { Button } from "@/components/ui/button";
+import { useCookies } from "react-cookie";
+
+interface IAddToBasketBtnProps {
+  productId: number;
+}
+
+const AddToBasketBtn: React.FC<IAddToBasketBtnProps> = ({ productId }) => {
+  const addProductToLocalStorage = () => {
+    addProductToLocalBasket(productId);
+  };
+  const addProductToServer = () => {};
+
+  const [cookies] = useCookies(["auth"]);
+
+  const addProductToBasket = () => {
+    const accountLoggedIn = cookies.auth !== undefined;
+    addProductToLocalStorage();
+    if (accountLoggedIn) {
+      addProductToServer();
+    }
+    alert(accountLoggedIn);
+  };
+
   return (
-    <Button variant={"secondary"} className="w-full">
+    <Button
+      variant={"secondary"}
+      className="w-full"
+      onClick={(event) => {
+        event.preventDefault();
+        addProductToBasket();
+      }}
+    >
       Add to basket
     </Button>
   );
