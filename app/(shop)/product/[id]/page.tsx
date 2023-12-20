9aca7fda0beb7a3d7d6adfcdb05c4e7c8025fa6b
Tobias <tobiasdunn.02@gmail.com>
2023-12-20 15:11:20 +0000

    Add generate static params for all product pages

--- app/(shop)/product/[id]/page.tsx
+++ app/(shop)/product/[id]/page.tsx
@@ -2,6 +2,16 @@ import { getProductImages } from "@/app/data/images";
 import { getProductWithId } from "@/app/data/products";
 import ProductImageDisplay from "./ProductImageDisplay";
 
+export async function generateStaticParams() {
+  const productIds = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_API_HOST_ADDRESS}/v1/products/all`
+  ).then((res) => res.json());
+
+  return productIds.map((product: { id: number }) => ({
+    id: product.id.toString(),
+  }));
+}
+
 const ProductPage = async ({ params }: { params: { id: number } }) => {
   const productDetails = await getProductWithId(params.id);
   const productImages = (await getProductImages(params.id)).map((image) => {
