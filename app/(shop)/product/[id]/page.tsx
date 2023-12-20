99ebcc01d894e6f57b07f2acec26b66771b8e391
Tobias <tobiasdunn.02@gmail.com>
2023-12-20 16:55:16 +0000

    Add customers almost looked at section

--- app/(shop)/product/[id]/page.tsx
+++ app/(shop)/product/[id]/page.tsx
@@ -2,11 +2,14 @@ import { getProductImages } from "@/app/data/images";
 import {
   getProductWithId,
   getProductsOfSameStyle,
+  getRandomProducts,
   mapProductsToImages,
 } from "@/app/data/products";
 import ProductImageDisplay from "./ProductImageDisplay";
 import MainProductSection from "./MainProductSection";
 import PurchaseSection from "./PurchaseSection";
+import ProductCarousel from "@/app/components/product-cards/ProductCarousel";
+import SectionHeading from "@/app/components/SectionHeading";
 
 export async function generateStaticParams() {
   const productIds = await fetch(
@@ -32,32 +35,50 @@ const ProductPage = async ({ params }: { params: { id: number } }) => {
     75,
     100
   );
+
+  // TODO - Make tailored to what customers actually looked at, rather than random
+  const customersAlsoLookedAt = await mapProductsToImages(
+    await getRandomProducts(25),
+    640,
+    853
+  );
+
   return (
-    <main className="flex flex-row w-full overflow-x-clip p-1 space-x-6">
-      <ProductImageDisplay images={productImages} />
-      <MainProductSection
-        productId={productDetails.id}
-        productName={productDetails.name}
-        productDescription={
-          productDetails.description === undefined
-            ? ""
-            : productDetails.description
-        }
-        productBrand={
-          productDetails.brandId === undefined ||
-          productDetails.brandName === undefined
-            ? undefined
-            : {
-                id: productDetails.brandId,
-                name: productDetails.brandName,
-              }
-        }
-      />
-      <div className="min-w-[300px] max-w-[300px]">
-        <PurchaseSection
+    <main className="flex flex-col w-full overflow-x-clip p-1 space-y-8">
+      <div className="flex flex-row w-full space-x-6">
+        <ProductImageDisplay images={productImages} />
+        <MainProductSection
           productId={productDetails.id}
-          price={productDetails.price}
-          similarStyleProducts={sameStyleProductsWithImages}
+          productName={productDetails.name}
+          productDescription={
+            productDetails.description === undefined
+              ? ""
+              : productDetails.description
+          }
+          productBrand={
+            productDetails.brandId === undefined ||
+            productDetails.brandName === undefined
+              ? undefined
+              : {
+                  id: productDetails.brandId,
+                  name: productDetails.brandName,
+                }
+          }
+        />
+        <div className="min-w-[300px] max-w-[300px]">
+          <PurchaseSection
+            productId={productDetails.id}
+            price={productDetails.price}
+            similarStyleProducts={sameStyleProductsWithImages}
+          />
+        </div>
+      </div>
+      <div className="flex flex-col space-y-4">
+        <SectionHeading text={"Customers also looked at"} />
+        <ProductCarousel
+          products={customersAlsoLookedAt}
+          imageWidth={640}
+          imageHeight={853}
         />
       </div>
     </main>
