59e13106f6b5f06a16b3cc6949d830472d7403ed
Tobias <tobiasdunn.02@gmail.com>
2023-12-29 18:21:08 +0000

    Add Address book to account settings

---
+++ app/(account)/settings/AddressForm.tsx
@@ -0,0 +1,247 @@
+"use client";
+
+import {
+  TCustomerAddress,
+  TShippingCountries,
+  createNewAddress,
+} from "@/app/data/address";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Loader2 } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z
+  .object({
+    firstAddressLine: z.string().min(1).trim(),
+    secondAddressLine: z.string().trim(),
+    areaCode: z.string().max(10).trim(),
+    state: z.string().min(1).trim(),
+    countryId: z.coerce
+      .number({ invalid_type_error: "Please select a country" })
+      .nonnegative(),
+  })
+  .required()
+  .partial({ secondAddressLine: true });
+
+interface IAddressFormProps {
+  countriesAvailable: TShippingCountries[];
+  refreshAddressData: () => void;
+}
+
+const AddressForm: React.FC<IAddressFormProps> = ({
+  countriesAvailable,
+  refreshAddressData,
+}) => {
+  const [confirmBoxOpen, setConfirmBoxOpen] = useState(false);
+  const [isMounted, setIsMounted] = useState(false);
+  const [addressAddedLoading, setAddressAddedLoading] = useState(false);
+  const selectRef: React.Ref<HTMLSpanElement> = useRef(null);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      firstAddressLine: "",
+      secondAddressLine: "",
+      areaCode: "",
+      state: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    const newAddress: TCustomerAddress = {
+      firstAddressLine: values.firstAddressLine,
+      secondAddressLine:
+        values.secondAddressLine === "" ? undefined : values.secondAddressLine,
+      areaCode: values.areaCode,
+      countryState: values.state,
+      countryId: values.countryId,
+    };
+    setAddressAddedLoading(true);
+    createNewAddress(newAddress)
+      .then((created) => {
+        if (created) {
+          form.reset();
+          form.setValue("countryId", values.countryId);
+          refreshAddressData();
+          setAddressAddedLoading(false);
+          setConfirmBoxOpen(true);
+        } else {
+          console.error("Address could not be created");
+          setAddressAddedLoading(false);
+        }
+      })
+      .catch((_) => {
+        setAddressAddedLoading(false);
+      });
+  }
+
+  return (
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
+          <FormField
+            control={form.control}
+            name="firstAddressLine"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-lg">
+                  First line of address *
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    type="text"
+                    placeholder="First line of address"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="secondAddressLine"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-lg">
+                  Second line of address *
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    type="text"
+                    placeholder="Second line of address"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="areaCode"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-lg">Area code*</FormLabel>
+                <FormControl>
+                  <Input type="text" placeholder="Area code" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="state"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-lg">State*</FormLabel>
+                <FormControl>
+                  <Input type="text" placeholder="State" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="countryId"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-lg">Country</FormLabel>
+                <FormControl>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={
+                      typeof field.value === "undefined"
+                        ? undefined
+                        : field.value.toString()
+                    }
+                  >
+                    <SelectTrigger>
+                      <SelectValue
+                        ref={selectRef}
+                        placeholder="Select a country"
+                      />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {countriesAvailable.map((country, index) => (
+                        <SelectItem
+                          className="cursor-pointer text-accent-foreground hover:text-secondary"
+                          key={index}
+                          value={country.id.toString()}
+                        >
+                          {country.name}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button
+            disabled={addressAddedLoading}
+            type="submit"
+            className="w-full text-base"
+            variant={"secondary"}
+          >
+            {addressAddedLoading ? (
+              <Loader2 className="w-4 h-4 mr-2 animate-spin" />
+            ) : (
+              <></>
+            )}
+            Add address
+          </Button>
+        </form>
+      </Form>{" "}
+      {isMounted ? (
+        <AlertDialog open={confirmBoxOpen} onOpenChange={setConfirmBoxOpen}>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Address created</AlertDialogTitle>
+            </AlertDialogHeader>
+            <AlertDialogAction className="bg-secondary">Ok</AlertDialogAction>
+          </AlertDialogContent>
+        </AlertDialog>
+      ) : (
+        <></>
+      )}
+    </>
+  );
+};
+
+export default AddressForm;
---
+++ app/data/address.ts
@@ -0,0 +1,225 @@
+export type TCustomerAddress = {
+  // The id of the addres
+  id?: number;
+  // The area code for the address
+  areaCode: string;
+  // The first line of the address
+  firstAddressLine: string;
+  // The state the address is in for the country
+  countryState: string;
+  // The id of the country the address is in
+  countryId: number;
+  // The name of the country
+  countryName?: string;
+  // The second line of the address, if it exists
+  secondAddressLine?: string;
+};
+
+export type TShippingCountries = {
+  id: number;
+  name: string;
+};
+
+export const getCountriesAvailableForShipping = (): Promise<
+  TShippingCountries[]
+> => {
+  return new Promise((resolve, reject) => {
+    fetch(
+      `${process.env.NEXT_PUBLIC_SERVER_API_HOST_ADDRESS}/v1/auth/address/available-countries`,
+      {
+        headers: {
+          "Content-type": "application/json",
+        },
+      }
+    )
+      .then((response) => {
+        if (response.ok) {
+          response
+            .json()
+            .then((jsonData) => {
+              resolve(jsonData);
+            })
+            .catch((err) => {
+              reject(err);
+            });
+        } else {
+          reject();
+        }
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+};
+
+export const getCustomerAddresses = (): Promise<TCustomerAddress[]> => {
+  return new Promise((resolve, reject) => {
+    fetch(
+      `${process.env.NEXT_PUBLIC_SERVER_API_HOST_ADDRESS}/v1/auth/address/`,
+      {
+        headers: {
+          "Content-type": "application/json",
+        },
+        credentials: "include",
+      }
+    )
+      .then((response) => {
+        if (response.ok) {
+          response
+            .json()
+            .then((jsonData) => {
+              resolve(jsonData);
+            })
+            .catch((err) => {
+              reject(err);
+            });
+        } else {
+          reject();
+        }
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+};
+
+export const getDefaultAddress = (): Promise<number | null> => {
+  return new Promise((resolve, reject) => {
+    fetch(
+      `${process.env.NEXT_PUBLIC_SERVER_API_HOST_ADDRESS}/v1/auth/address/default`,
+      {
+        headers: {
+          "Content-type": "application/json",
+        },
+        credentials: "include",
+      }
+    )
+      .then((response) => {
+        if (response.ok) {
+          response
+            .json()
+            .then((jsonData) => {
+              if (jsonData === null) {
+                resolve(null);
+              } else {
+                resolve(jsonData.id);
+              }
+            })
+            .catch((err) => reject(err));
+        } else {
+          reject();
+        }
+      })
+      .catch((err) => reject(err));
+  });
+};
+
+export const serverUpdateDefaultAddress = (
+  addressId: number
+): Promise<boolean> => {
+  return new Promise((resolve, reject) => {
+    fetch(
+      `${process.env.NEXT_PUBLIC_SERVER_API_HOST_ADDRESS}/v1/auth/address/default`,
+      {
+        headers: {
+          "Content-type": "application/json",
+        },
+        credentials: "include",
+        method: "PUT",
+        body: JSON.stringify({
+          addressId: addressId,
+        }),
+      }
+    )
+      .then((response) => {
+        resolve(response.ok);
+      })
+      .catch((err) => {
+        console.error(err);
+        reject(err);
+      });
+  });
+};
+
+export const serverSetDefaultAddress = (
+  addressId: number
+): Promise<boolean> => {
+  return new Promise((resolve, reject) => {
+    fetch(
+      `${process.env.NEXT_PUBLIC_SERVER_API_HOST_ADDRESS}/v1/auth/address/default`,
+      {
+        headers: {
+          "Content-type": "application/json",
+        },
+        credentials: "include",
+        method: "POST",
+        body: JSON.stringify({
+          addressId: addressId,
+        }),
+      }
+    )
+      .then((response) => {
+        resolve(response.ok);
+      })
+      .catch((err) => {
+        console.error(err);
+        reject(err);
+      });
+  });
+};
+
+export const serverDeleteAddress = (addressId: number): Promise<boolean> => {
+  return new Promise((resolve, reject) => {
+    fetch(
+      `${process.env.NEXT_PUBLIC_SERVER_API_HOST_ADDRESS}/v1/auth/address/`,
+      {
+        headers: {
+          "Content-type": "application/json",
+        },
+        credentials: "include",
+        method: "DELETE",
+        body: JSON.stringify({
+          addressId: addressId,
+        }),
+      }
+    )
+      .then((response) => {
+        resolve(response.ok);
+      })
+      .catch((err) => {
+        console.error(err);
+        reject(err);
+      });
+  });
+};
+
+export const createNewAddress = (
+  address: TCustomerAddress
+): Promise<boolean> => {
+  return new Promise((resolve, reject) => {
+    fetch(
+      `${process.env.NEXT_PUBLIC_SERVER_API_HOST_ADDRESS}/v1/auth/address/`,
+      {
+        headers: {
+          "Content-type": "application/json",
+        },
+        credentials: "include",
+        method: "POST",
+        body: JSON.stringify({
+          "first-address-line": address.firstAddressLine,
+          "second-address-line": address.secondAddressLine,
+          "area-code": address.areaCode,
+          state: address.countryState,
+          "country-id": address.countryId,
+        }),
+      }
+    )
+      .then((response) => {
+        resolve(response.ok);
+      })
+      .catch((err) => {
+        console.error(err);
+        reject(err);
+      });
+  });
+};
