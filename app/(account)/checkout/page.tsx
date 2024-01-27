0c53f6c79655afc3e89953471cb4f955c9096abd
Tobias <tobiasdunn.02@gmail.com>
2024-01-27 15:06:57 +0000

    Initial page for checkout

---
+++ app/(account)/checkout/page.tsx
@@ -0,0 +1,11 @@
+import SectionHeading from "@/app/components/SectionHeading";
+
+const Page = () => {
+  return (
+    <main className="flex flex-col w-full space-y-4 mt-4">
+      <SectionHeading text={"Checkout"} />
+    </main>
+  );
+};
+
+export default Page;
