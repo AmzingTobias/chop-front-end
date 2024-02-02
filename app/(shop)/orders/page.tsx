06193f1f371e1b2f44ce2493f873d6a8d641ce0f
Tobias <40802800+AmzingTobias@users.noreply.github.com>
2024-02-02 08:38:04 +0000

    Move orders section to (shop)

--- app/(shop)/orders/page.tsx
+++ app/(shop)/orders/page.tsx
@@ -3,7 +3,7 @@ import CustomersOrders from "./CustomersOrders";
 
 const Page = () => {
   return (
-    <main className="flex flex-col w-full p-4 mx-auto max-w-screen-2xl">
+    <main className="flex flex-col w-full p-4 mx-auto max-w-screen-2xl gap-4">
       <SectionHeading text="Your orders" />
       <CustomersOrders />
     </main>
