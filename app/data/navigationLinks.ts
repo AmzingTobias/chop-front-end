4ee0ca48c1cf30f9cb685136bdc26a775cd554a7
Tobias <40802800+AmzingTobias@users.noreply.github.com>
2024-02-15 11:46:33 +0000

    Order nav links by product count

--- app/data/navigationLinks.ts
+++ app/data/navigationLinks.ts
@@ -9,6 +9,7 @@ export const getProductTypesNavLinks = async (): Promise<
   TNavigationLinks[]
 > => {
   const data = await getProductTypes();
+  data.sort((a, b) => b.productCount - a.productCount);
   return data.map((productType) => ({
     displayName: productType.type,
     path: `/product-type/${productType.id}`,
