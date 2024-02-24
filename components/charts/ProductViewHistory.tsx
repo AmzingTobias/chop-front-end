f5c2c4093a05693e2dc39ba445d111dacdaed45f
Tobias <40802800+AmzingTobias@users.noreply.github.com>
2024-02-24 16:41:16 +0000

    Create chart to show product view history

---
+++ components/charts/ProductViewHistory.tsx
@@ -0,0 +1,150 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import {
+  ChartData,
+  Chart as ChartJS,
+  ChartOptions,
+  registerables,
+} from "chart.js";
+import { Doughnut } from "react-chartjs-2";
+import SectionHeading from "@/app/components/SectionHeading";
+import { DateRange } from "react-day-picker";
+import DatePicker from "./DatePicker";
+
+type TProductViewHistory = {
+  productId: number;
+  name: string;
+  viewCount: number;
+};
+const fetchData = (
+  startDate?: Date,
+  endDate?: Date
+): Promise<TProductViewHistory[]> => {
+  return new Promise((resolve, reject) => {
+    fetch(
+      `${
+        process.env.NEXT_PUBLIC_SERVER_API_HOST_ADDRESS
+      }/v1/analytics/product-view-history${startDate || endDate ? "?" : ""}${
+        startDate ? "startDate=" + startDate.toISOString().split("T")[0] : ""
+      }${startDate && endDate ? "&" : ""}${
+        endDate ? "endDate=" + endDate.toISOString().split("T")[0] : ""
+      }`,
+      {
+        mode: "cors",
+        credentials: "include",
+        method: "GET",
+      }
+    )
+      .then((response) => {
+        if (response.ok) {
+          response
+            .json()
+            .then((data) => resolve(data))
+            .catch((err) => reject(err));
+        } else {
+          reject("Failed to get purchase analytics");
+        }
+      })
+      .catch((err) => reject(err));
+  });
+};
+
+const ProductViewHistory = () => {
+  const useData = () => {
+    const [data, setData] = useState<TProductViewHistory[]>([]);
+    const refreshData = (startDate?: Date, endDate?: Date) => {
+      fetchData(startDate, endDate)
+        .then((dataFetched) => {
+          console.log(dataFetched.length);
+          if (dataFetched.length > 5) {
+            const topValues = dataFetched.slice(0, 5);
+            const otherCount = dataFetched
+              .slice(5)
+              .reduce((sum, entry) => sum + entry.viewCount, 0);
+            const condensedData: TProductViewHistory[] = [
+              ...topValues,
+              { name: "Other", productId: -1, viewCount: otherCount },
+            ];
+            setData(condensedData);
+          } else {
+            setData(dataFetched);
+          }
+        })
+
+        .catch((err) => {
+          console.error(err);
+          setData([]);
+        });
+    };
+    useEffect(() => {
+      refreshData();
+    }, []);
+    return { data, refreshData };
+  };
+  const { data: productViewHistoryData, refreshData } = useData();
+  const [dateRange, setDateRange] = useState<DateRange | undefined>({
+    from: new Date(),
+    to: new Date(new Date().setMonth(new Date().getMonth() - 1)),
+  });
+  useState(productViewHistoryData);
+
+  useEffect(() => {
+    refreshData(dateRange?.from, dateRange?.to);
+  }, [dateRange, refreshData]);
+
+  const chartData: ChartData<"doughnut"> = {
+    labels: productViewHistoryData.map((entry) => entry.name),
+    datasets: [
+      {
+        data: productViewHistoryData.map((entry) => entry.viewCount),
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+      },
+    ],
+  };
+
+  const chartOptions: ChartOptions<"doughnut"> = {
+    plugins: {
+      legend: {
+        display: true,
+        labels: {
+          color: "#FFFFFF",
+        },
+        onClick: () => {},
+      },
+    },
+  };
+
+  ChartJS.register(...registerables);
+
+  return (
+    <div className=" h-fit w-full bg-accent text-accent-foreground p-4 rounded-md flex flex-col gap-2">
+      <SectionHeading text="View history" />
+      <Doughnut data={chartData} options={chartOptions} />
+      <div className="flex flex-row gap-2 w-full justify-center">
+        <DatePicker
+          dateRange={dateRange}
+          fromDate={undefined}
+          setDateRange={setDateRange}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ProductViewHistory;
