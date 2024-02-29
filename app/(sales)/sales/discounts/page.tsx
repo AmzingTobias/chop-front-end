f40d966d46e1e64329a3b73c91b2aa9e1999d86d
Tobias <40802800+AmzingTobias@users.noreply.github.com>
2024-02-29 12:11:46 +0000

    Add discounts page

---
+++ app/(sales)/sales/discounts/page.tsx
@@ -0,0 +1,11 @@
+import DiscountsContent from "../../../(admin)/admin/discounts/DiscountsContent";
+
+const Page = () => {
+  return (
+    <main className="w-full h-screen max-h-screen overflow-clip flex flex-row p-4">
+      <DiscountsContent />
+    </main>
+  );
+};
+
+export default Page;
