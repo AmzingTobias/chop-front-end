b299044f52b4f82d24a1305cf01f146b7d529c0a
Tobias <tobiasdunn.02@gmail.com>
2024-03-19 10:22:18 +0000

    Add dummy delivery and returns page

---
+++ app/(customerFacing)/(shop)/delivery/page.tsx
@@ -0,0 +1,11 @@
+import Delivery from "./Delivery";
+
+const Page = () => {
+  return (
+    <main className="flex flex-col w-full p-4 mx-auto max-w-screen-2xl gap-4">
+      <Delivery />
+    </main>
+  );
+};
+
+export default Page;
